60b6f8bee4d4324254587bfccc374073fe7ad72f
cyamy <hi.cyamy@gmail.com>
2022-08-24 20:03:13 +0900

    feat: add react-testing-library

---
+++ src/sample/MyComponent.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { MyComponent } from './MyComponent'
+
+test('render Hello Test', () => {
+  render(<MyComponent />)
+  screen.debug()
+  expect(screen.getByText('Hello Test')).toBeInTheDocument()
+})
