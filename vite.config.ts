fd4b5974d114aaced9f42fa2c5ddb3744a534711
cyamy <hi.cyamy@gmail.com>
2022-09-22 22:24:51 +0900

    feat: emotion setting for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts'
+    setupFiles: './vitest.setup.ts',
   },
 });
