1d35dfbc87b9cbb3e64624eb19739bb36e744b56
cyamy <hi.cyamy@gmail.com>
2022-09-22 23:16:23 +0900

    fix: update testing environment

--- src/components/sample/index.test.tsx
+++ src/components/sample/index.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import { MyComponent } from './MyComponent'
+import { render, screen } from '@testing-library/react';
+import { MyComponent } from './index';
 
 test('render Hello Test', () => {
-  render(<MyComponent />)
-  screen.debug()
-  expect(screen.getByText('Hello Test')).toBeInTheDocument()
-})
+  render(<MyComponent />);
+  screen.debug();
+  expect(screen.getByText('Hello Test')).toBeInTheDocument();
+});
